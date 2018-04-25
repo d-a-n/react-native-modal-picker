a702da6b81e7f388eaea34dc286966650cc85676
ktownsend-cbre <37886142+ktownsend-cbre@users.noreply.github.com>
2018-04-25 16:20:44 -0500

    fix PropTypes import for React 15.5
    As of react 15.5, PropTypes has moved to its own library.
    Also fixed a warning about keyboardShouldPersistTaps needing to be "always" rather than {true}.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
-import React,{
-    PropTypes
-} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import {
     View,
@@ -131,7 +130,7 @@ export default class ModalPicker extends BaseComponent {
         return (
             <View style={[styles.overlayStyle, this.props.overlayStyle]} key={'modalPicker'+(componentIndex++)}>
                 <View style={styles.optionContainer}>
-                    <ScrollView keyboardShouldPersistTaps>
+                    <ScrollView keyboardShouldPersistTaps="always">
                         <View style={{paddingHorizontal:10}}>
                             {options}
                         </View>
